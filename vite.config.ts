4a74ad14fa6bfdeb7f7fb76df3ceb036631021be
scorfie <samithacoc@gmail.com>
2026-03-04 16:31:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://aws-face-api.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  plugins: [react()]
 })
